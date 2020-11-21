21059ca6c52ef08e79a2d534b49fe0c76a5141d3
khanr <khanriza@gmail.com>
2020-11-21 17:59:53 -0500

    name updated

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">portfolio</h1>
+      <h1 class="title">Jawads Portfolio</h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
